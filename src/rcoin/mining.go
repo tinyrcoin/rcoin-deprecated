c743a559cac5dac30254a466cdf30323a4edd799
Ronsor <ronsor@ronsor.cu.cc>
2017-12-21 05:03:52 +0000

    fixes

---
+++ src/rcoin/mining.go
@@ -0,0 +1,40 @@
+
+package main
+import "time"
+import "log"
+func GetTransactions() []Transaction {
+	if len(unconfirmed) == 0 { return nil }
+	ret := []Transaction(nil)
+	del := []string(nil)
+	for k, v := range unconfirmed {
+		if len(ret) > 85 { break }
+		ret = append(ret, *v)
+		del = append(del, k)
+	}
+	for _, v := range del {
+		delete(unconfirmed, v)
+	}
+	return ret
+}
+
+func Miner(threads int, payout []byte) {
+	log.Println("Starting miner")
+	log.Printf("Rewards go to %s", Address(payout[32:]).String())
+	for {
+		txs := GetTransactions()
+		if txs == nil { time.Sleep(1*time.Second); continue }
+		log.Printf("Working on block with %d transactions.", len(txs))
+		b := NewBlock()
+		b.TX = txs
+		b.ProofOfWork(chain.GetDifficulty(), threads)
+		b.Time = time.Now().Unix()
+		b.Sign(payout)
+		if !b.Verify() { panic("Block verification error") }
+		if !chain.Verify(b) {
+			log.Println("Someone beat me to this block.")
+			continue
+		}
+		Broadcast(Command{Type:CMD_BLOCK,Block:*b}, "")
+		chain.AddBlock(b)
+	}
+}
